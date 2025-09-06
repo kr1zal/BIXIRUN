d804ba6a98ea9fce6a2d10428d5d0aa2c5a4c20c
kr1zal <kr1zal@users.noreply.github.com>
2025-09-07 02:06:32 +0300

    fix: Исправление .gitignore и удаление лишних файлов
    - Добавлен полный .gitignore для React Native/Expo проекта
    - Удалены node_modules/, .expo/, build/ папки из репозитория
    - Теперь GitHub будет показывать правильный языковой состав
    
    Готово к загрузке на TestFlight

--- ios/Pods/Headers/Public/ExpoScreenCapture/ExpoScreenCapture-umbrella.h
+++
@@ -1,1 +0,0 @@
-../../../Target Support Files/ExpoScreenCapture/ExpoScreenCapture-umbrella.h
--- ios/Pods/Target Support Files/ExpoLocalAuthentication/ExpoLocalAuthentication-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_ExpoLocalAuthentication : NSObject
-@end
-@implementation PodsDummy_ExpoLocalAuthentication
-@end
